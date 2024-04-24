ed894464d1ea221c4788aa1c969c062197547f53
Ferdy Hahan Pradana <ferdyhahan5@gmail.com>
2024-04-25 04:50:59 +0700

    fix to upload

--- curl_action.php
+++ curl_action.php
@@ -1,10 +1,8 @@
 <?php
-session_start();
-
 $nim_nama = $_POST["nim-nama"];
+
+// Execute curl command to fetch data
 $result = shell_exec('curl -k "https://siakad.polinema.ac.id/ajax/ms_mhs/cari_mhs?q=' . urlencode($nim_nama) . '"');
 
-$_SESSION['finalresult'] = $result;
-
-
-header('location: result.php');
+// Output the result
+echo $result;
--- index.php
+++ index.php
@@ -4,24 +4,48 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Find My NIM</title>
-    <link rel="stylesheet" href="./assets/vendor/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <title>FindYourNIM</title>
+    <link rel="stylesheet" href="./assets/vendor/tailwind/tailwind.min.css">
     <script src="./assets/vendor/jquery/jquery.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <link rel="stylesheet" href="./assets/vendor/datatable/jquery.dataTables.min.css">
+    <script src="./assets/vendor/datatable/jquery.dataTables.min.js"></script>
 </head>
 
 <body>
-    <div class="container my-5">
-        <form id="search-form" action="curl_action.php" method="post">
-            <div class="mb-3">
-                <input type="nim-nama" class="form-control" placeholder="Masukkan Nim atau Nama" name="nim-nama">
+    <div class="mx-auto w-5/6 md:w-4/6 my-5 pb-12">
+
+        <!-- header  -->
+        <div class="flex justify-between my-4 items-center">
+            <h1 class="text-center font-medium text-3xl">FindYourNIM</h1>
+            <div class="card-header text-muted flex gap-2 justify-between">
+                <a href="https://github.com/ferdyhape" class="my-auto text-dark" target="_blank">
+                    <img src="./assets/icon/github.svg" alt="" srcset="">
+                </a>
+                <a href="https://www.linkedin.com/in/ferdyhape/" class="my-auto text-dark" target="_blank">
+                    <img src="./assets/icon/linkedin.svg" alt="" srcset="">
+                </a>
+                <a href="https://www.instagram.com/ferdyhape/" class="my-auto text-dark" target="_blank">
+                    <img src="./assets/icon/instagram.svg" alt="" srcset="">
+                </a>
             </div>
-            <input type="submit" class="btn btn-primary">
+        </div>
+
+        <!-- form input nim nama -->
+        <form id="search-form" method="post" class="w-full">
+            <div class="flex justify-between">
+                <input type="text" class="w-full border border-gray-300 rounded-md py-2 px-4 mr-3" placeholder="Masukkan Inisiasi Nim atau Nama" name="nim-nama">
+                <input type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" value="Cari">
+            </div>
         </form>
 
-        <div class="result-table">
-            <table class="table table-bordered">
+
+        <!-- result table -->
+        <div class="result-table mt-5 table-responsive">
+            <div id="loading" style="display: none" class="flex justify-center items-center gap-x-3">
+                <img src="./assets/icon/loading.gif" alt="Loading...">
+                <h3>Loading ...</h3>
+            </div>
+            <table id="result-table" class="table">
                 <thead>
                     <tr>
                         <th>NIM</th>
@@ -32,16 +56,62 @@
                 </tbody>
             </table>
         </div>
+
+
+        <!-- footer -->
+        <div class="fixed bottom-0 w-5/6 md:w-4/6 bg-gradient-to-r from-blue-300 to-blue-400 p-3 text-center rounded-t-lg">
+            <p class="text-sm">
+                Another of my "Gabut Project" - <a href="https://cekkompen.isdeilabs.com/" class="text-blue-900 underline font-bold" target="_blank">CekKompen</a>
+            </p>
+        </div>
+
     </div>
+
     <script>
         $(document).ready(function() {
+            $('#result-table').css('display', 'none');
             $('#search-form').submit(function(e) {
-                let nimNama = $(this).find('input').val();
-                // buka https://siakad.polinema.ac.id/ajax/ms_mhs/cari_mhs?q=${nimNama} kemudian ambil data dalam bentuk html
+                e.preventDefault();
+                let nimNama = $(this).find('input[name="nim-nama"]').val();
+                // if table exist then destroy and hide
+                if ($.fn.DataTable.isDataTable('#result-table')) {
+                    $('#result-table').DataTable().destroy();
+                    $('#result-table').css('display', 'none');
+                }
+                $('#loading').show();
+                $.ajax({
+                    url: 'curl_action.php',
+                    type: 'POST',
+                    data: {
+                        'nim-nama': nimNama
+                    },
+                    success: function(response) {
+                        // Hide loading indicator
+                        $('#loading').hide();
+                        let data = JSON.parse(response);
+                        $('#result-table tbody').empty();
+                        $.each(data, function(index, item) {
+                            let textParts = item.text.split(" - ");
+                            let nim = textParts[0];
+                            let nama = textParts[1];
+                            $('#result-table tbody').append('<tr><td>' + nim + '</td><td>' + nama + '</td></tr>');
+                        });
+
+                        // Show the table
+                        $('#result-table').css('display', 'table');
+
+                        // Initialize DataTables
+                        $('#result-table').DataTable();
+                    },
+                    error: function(xhr, status, error) {
+                        console.error(error);
+                        // Hide loading indicator
+                        $('#loading').hide();
+                    }
+                });
             });
         });
     </script>
-    <script src="./assets/vendor/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
